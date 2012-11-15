73c586a4a2157b5a950472ba5f43946b70bfd879
Prasanjit Singh <scribblelead@gmail.com>
2012-11-15 17:06:00 +0530

    Not tested yet

---
+++ webhits.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# webhits -- Reports amount of website hits, total values and per user hits by parsing the Apache Access log
+# Compiled By: Prasanjit 
+#
+########
+# Declare the apache access log path by either using 
+WEBLOG="/var/log/httpd/access_log"
+
+if [ "$1" = "" ]; then
+ echo "Usage: `basename $0` <user1> <user2> osv.."
+ exit
+fi
+date > wtstat
+echo "`cat $WEBLOG|wc -l` total hits on `hostname` " >>wtstat
+function_treff() {
+ echo "`grep $i $WEBLOG|wc -l` hits on $i" >>wtstat
+}
+for i in $* 
+do
+ function_treff
+done 
+sort -r wtstat -o wtstat
+more wtstat
+rm wtstat
